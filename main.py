10197cec0cc51c892a48155522496aab42f9d72a
AleksandraX <110120649+demonic-katana@users.noreply.github.com>
2022-11-12 16:59:58 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 import sqlite3
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QAction, QFileDialog, QDialog, QDialogButtonBox, \
-    QLabel, QVBoxLayout
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette, QImage, QBrush
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QInputDialog, QWidget, QDialog
 from PyQt5 import uic
 
 
@@ -9,26 +11,76 @@ class OwnWindow(QMainWindow):
     def __init__(self, db):
         self.db = db
         super().__init__()
-        self._window = ''
-        uic.loadUi('interface\\diary.ui', self)
+        self.name_window = ''
+        uic.loadUi('interface/diary.ui', self)
+        self.setWindowTitle("Book of Destiny")
         # настройка компонентов:
         # настройка кнопок
         self.leave.clicked.connect(self._leave)
+        self.settings.clicked.connect(self._settings)
+
+    def _settings(self):
+        self.settings = WindowSetting()
+        self.settings.show()
 
     def _leave(self):
-        self._window = 'hello'
+        self.name_window = 'hello'
         self.close()
 
     def getData(self):
-        return self._window,
+        return self.name_window,
+
+    def resizeEvent(self, event):
+        try:
+            palette = QPalette()
+            img = QImage(background_image)
+            scaled = img.scaled(self.size(), Qt.KeepAspectRatioByExpanding, transformMode=Qt.SmoothTransformation)
+            palette.setBrush(QPalette.Window, QBrush(scaled))
+            self.setPalette(palette)
+        except Exception:
+            pass
 
 
-class WindowSelect(QMainWindow):
+class WindowGreeting(QMainWindow):
+    def __init__(self):
+        self.name_window = ''
+        super().__init__()
+        uic.loadUi('interface/hello.ui', self)
+        self.setWindowTitle("Book of Destiny: Добро пожаловать")
+        # Настройки кнопок
+        [i.clicked.connect(self.run) for i in [self.select, self.create, self.settings]]
+
+    def run(self):
+        if self.sender().text() == 'Создать':
+            self.name_window = 'createBook'
+            self.close()
+        elif self.sender().text() == 'Выбрать':
+            self.name_window = 'selectBook'
+            self.close()
+        elif self.sender().text() == 'Настройки':
+            self.second_window = WindowSetting()
+            self.second_window.show()
+
+    def getData(self):
+        return self.name_window,
+
+    def resizeEvent(self, event):
+        try:
+            palette = QPalette()
+            img = QImage(background_image)
+            scaled = img.scaled(self.size(), Qt.KeepAspectRatioByExpanding, transformMode=Qt.SmoothTransformation)
+            palette.setBrush(QPalette.Window, QBrush(scaled))
+            self.setPalette(palette)
+        except Exception:
+            pass
+
+
+class WindowSelect(QWidget):
     def __init__(self):
         super().__init__()
-        self._window = ''
-        uic.loadUi('interface\\selectBook.ui', self)
-        self.setWindowTitle("Book of Destiny")
+        self.name_window = ''
+        uic.loadUi('interface/selectBook.ui', self)
+        self.setWindowTitle("Book of Destiny: Выбрать")
         # Настройки кнопок
         self.dialog_sel.clicked.connect(self._filedialog)
         self.cancel.clicked.connect(self._cancel)
@@ -36,10 +88,9 @@ class WindowSelect(QMainWindow):
         self.database = None
 
     def _filedialog(self):
-        self.Put.setText(QFileDialog.getOpenFileName(self,
-                                                          'Выбрать книгу',
-                                                          '',
-                                                          'Data Base file (*.db);;Все файлы (*)')[0])
+        self.Put.setText(QFileDialog.getOpenFileName(self, 'Выбрать книгу',
+                                                           '',
+                                                           'Data Base file (*.db);;Все файлы (*)')[0])
 
     def _select(self):
         database = self.Put.text()
@@ -48,67 +99,115 @@ class WindowSelect(QMainWindow):
                 f = open(database, mode='r', encoding='utf8')
                 f.close()
                 self.database = database
-                self._window = 'diary'
+                self.name_window = 'diary'
                 self.close()
             except Exception:
                 self.statusBar().showMessage("База данных не найдена.")
         else:
             self.statusBar().showMessage("Не правильное имя базы данных.")
 
+    def closeEvent(self, event):
+        if not self.name_window:
+            self.name_window = 'hello'
+
     def _cancel(self):
-        self._window = 'hello'
+        self.name_window = 'hello'
+        self.close()
+
+    def _leave(self):
+        self.name_window = 'hello'
         self.close()
 
     def getData(self):
-        return self._window, self.database
+        return self.name_window, self.database
 
-class WindowGreeting(QMainWindow):
+
+class WindowCreate(QWidget):
     def __init__(self):
-        self._window = ''
         super().__init__()
-        uic.loadUi('interface\\hello.ui', self)
-        self.setWindowTitle("Book of Destiny")
-        # Настройки кнопок
-        self.select.clicked.connect(self._select)
-        self.create.clicked.connect(self._create)
-        self.settings.clicked.connect(self._settings)
+        self.name_window = ''
+        uic.loadUi('interface/createBook.ui', self)
+        self.setWindowTitle("Book of Destiny: Создать")
 
-    def _select(self):
-        self._window = 'selectBook'
+
+    def _cancel(self):
+        self.name_window = 'hello'
         self.close()
 
-    def _create(self):
-        self._window = 'createBook'
+    def _leave(self):
+        self.name_window = 'hello'
         self.close()
 
-    def _settings(self):
-        self._window = 'settings_h'
-        self.close()
+    def closeEvent(self, event):
+        if not self.name_window:
+            self.name_window = 'hello'
 
     def getData(self):
-        return self._window,
+        return self.name_window, self.database
+
+
+class WindowSetting(QDialog):
+    def __init__(self):
+        self.name_window = ''
+        super().__init__()
+        uic.loadUi('interface/diary_settings.ui', self)
+        self.setWindowTitle("Book of Destiny: Настройки")
+        # self.b_w_theme.clicked.connect(self._create)
+
+    def _sort(self):
+        argument, ok_pressed = QInputDialog.getItem(
+            self, "Выберите как отсортировать записи", "Параметр сортировки записей:",
+            ("ID", "Название", "Дата создания"), 0, False)
+        if ok_pressed:
+            print(argument)
+
+    def _background_image(self):
+        global background_image
+        background_image = QFileDialog.getOpenFileName(
+            self, 'Выбрать картинку', '', 'Картинка (*.jpg);;Картинка (*.png);;Все файлы (*)')[0]
+
+    def closeEvent(self, event):
+        self.name_window = 'hello'
+
+    def resizeEvent(self, event):
+        try:
+            palette = QPalette()
+            img = QImage(background_image)
+            scaled = img.scaled(self.size(), Qt.KeepAspectRatioByExpanding, transformMode=Qt.SmoothTransformation)
+            palette.setBrush(QPalette.Window, QBrush(scaled))
+            self.setPalette(palette)
+        except Exception:
+            pass
+
+    def getData(self):
+        return self.name_window,
+
+
+def excepthook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    _window = 'hello'
-    while _window != '':
+    name_window = 'hello'
+    background_image = r'E:\Pictures\Фоновые изображения рабочего стола\911045.png'
+    sys.excepthook = excepthook
+    while name_window != '':
         try:
-            if _window == 'hello':
+            if name_window == 'hello':
                 window = WindowGreeting()
-            elif _window == 'selectBook':
+            elif name_window == 'selectBook':
                 window = WindowSelect()
-            elif _window == 'createBook':
-                pass
-            elif _window == 'settings_h':
-                pass
-            elif _window == 'diary':
+            elif name_window == 'createBook':
+                window = WindowCreate()
+            elif name_window == 'diary':
                 window = OwnWindow(db)
             window.show()
             s = app.exec()
             data = window.getData()
-            if _window == 'selectBook':
+            if name_window == 'selectBook':
                 db = data[1]
-            _window = data[0]
+            name_window = data[0]
         except Exception as es:
             sys.exit(es)
     sys.exit(0)
